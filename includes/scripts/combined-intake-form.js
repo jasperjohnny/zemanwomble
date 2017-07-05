40743494179daf86f0a6bc95d94871df53deb2d5
jasperjohnny <johnklingler@yahoo.com>
2017-07-05 01:16:43 -0400

    cleans up forms

--- includes/scripts/combined-intake-form.js
+++ includes/scripts/combined-intake-form.js
@@ -5,26 +5,26 @@ if (!$('script[src$="autofillFSP.js"]:first').attr('hasexecuted')) {
 	$(document).ready(function(){
 		$('.formAutoFill').each(function(){
 			// Bind focus event to inputs & textareas
-			$(this).find('input[type="text"],textarea').focus(function(){
-				if ($(this).val() == $(this).attr('title')) $(this).val('');
-				$(this).css({ fontWeight: 'normal' });
-			});
+			// $(this).find('input[type="text"],textarea').focus(function(){
+			// 	if ($(this).val() == $(this).attr('title')) $(this).val('');
+			// 	$(this).css({ fontWeight: 'normal' });
+			// });
 			
 			// Bind blur event to inputs & texteareas
-			$(this).find('input:text,textarea').blur(function(){
-				if (!$(this).val()) $(this).val($(this).attr('title'));
-				if ($(this).val() == $(this).attr('title')) {
-					if ($(this).parent().parent().find('label[for="' + $(this).attr('id') + '"] strong').length) $(this).css({ fontWeight: 'bold' });
-				}
-			});
+			// $(this).find('input:text,textarea').blur(function(){
+			// 	if (!$(this).val()) $(this).val($(this).attr('title'));
+			// 	if ($(this).val() == $(this).attr('title')) {
+			// 		if ($(this).parent().parent().find('label[for="' + $(this).attr('id') + '"] strong').length) $(this).css({ fontWeight: 'bold' });
+			// 	}
+			// });
 			
 			// Hide labels and auto-fill inputs
-			$(this).find('input[type="text"],textarea').each(function(){
-				var myLabel = $(this).parent().parent().find('label[for="' + $(this).attr('id') + '"]:not(.formHide)');
-				if (!myLabel.find('input[type="text"]').length) myLabel.addClass('formLabelHide');
-				$(this).val($(this).attr('title'));
-				if (myLabel.find('strong').length) $(this).css({ fontWeight: 'bold' });
-			});
+			// $(this).find('input[type="text"],textarea').each(function(){
+			// 	var myLabel = $(this).parent().parent().find('label[for="' + $(this).attr('id') + '"]:not(.formHide)');
+			// 	if (!myLabel.find('input[type="text"]').length) myLabel.addClass('formLabelHide');
+			// 	$(this).val($(this).attr('title'));
+			// 	if (myLabel.find('strong').length) $(this).css({ fontWeight: 'bold' });
+			// });
 		});
 	});
 
